a16a88f1ecc28a6e4c52a1eaa61e823fad0e78e8
Dmitry Veltishchev <vdm-photo@ya.ru>
2017-06-18 21:48:58 +0300

    +openal and portaudio

--- build-telegram-desktop.sh
+++ build-telegram-desktop.sh
@@ -109,7 +109,7 @@ prepare_source_from_git()
 
 inst git
 inst g++
-inst libtool automake autoconf build-essential
+inst libtool automake autoconf build-essential pkg-config
 inst wget
 
 # root directory for all stuff
@@ -174,15 +174,33 @@ if ! already_done libvdpau; then
 fi
 
 if ! already_done ffmpeg; then
-	prepare_source_from_git ffmpeg "https://github.com/FFmpeg/FFmpeg.git" ffmpeg
+	prepare_source_from_git ffmpeg "https://github.com/FFmpeg/FFmpeg.git"
 	git checkout "release/3.2"
 
-	inst libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
+	inst libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev texi2html zlib1g-dev
 	inst yasm
 
 	./configure --prefix=/usr/local --disable-programs --disable-doc --disable-everything --enable-protocol=file --enable-libopus --enable-decoder=aac --enable-decoder=aac_latm --enable-decoder=aasc --enable-decoder=flac --enable-decoder=gif --enable-decoder=h264 --enable-decoder=h264_vdpau --enable-decoder=mp1 --enable-decoder=mp1float --enable-decoder=mp2 --enable-decoder=mp2float --enable-decoder=mp3 --enable-decoder=mp3adu --enable-decoder=mp3adufloat --enable-decoder=mp3float --enable-decoder=mp3on4 --enable-decoder=mp3on4float --enable-decoder=mpeg4 --enable-decoder=mpeg4_vdpau --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 --enable-decoder=opus --enable-decoder=pcm_alaw --enable-decoder=pcm_alaw_at --enable-decoder=pcm_f32be --enable-decoder=pcm_f32le --enable-decoder=pcm_f64be --enable-decoder=pcm_f64le --enable-decoder=pcm_lxf --enable-decoder=pcm_mulaw --enable-decoder=pcm_mulaw_at --enable-decoder=pcm_s16be --enable-decoder=pcm_s16be_planar --enable-decoder=pcm_s16le --enable-decoder=pcm_s16le_planar --enable-decoder=pcm_s24be --enable-decoder=pcm_s24daud --enable-decoder=pcm_s24le --enable-decoder=pcm_s24le_planar --enable-decoder=pcm_s32be --enable-decoder=pcm_s32le --enable-decoder=pcm_s32le_planar --enable-decoder=pcm_s64be --enable-decoder=pcm_s64le --enable-decoder=pcm_s8 --enable-decoder=pcm_s8_planar --enable-decoder=pcm_u16be --enable-decoder=pcm_u16le --enable-decoder=pcm_u24be --enable-decoder=pcm_u24le --enable-decoder=pcm_u32be --enable-decoder=pcm_u32le --enable-decoder=pcm_u8 --enable-decoder=pcm_zork --enable-decoder=vorbis --enable-decoder=wavpack --enable-decoder=wmalossless --enable-decoder=wmapro --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmavoice --enable-encoder=libopus --enable-hwaccel=h264_vaapi --enable-hwaccel=h264_vdpau --enable-hwaccel=mpeg4_vaapi --enable-hwaccel=mpeg4_vdpau --enable-parser=aac --enable-parser=aac_latm --enable-parser=flac --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mpegaudio --enable-parser=opus --enable-parser=vorbis --enable-demuxer=aac --enable-demuxer=flac --enable-demuxer=gif --enable-demuxer=h264 --enable-demuxer=mov --enable-demuxer=mp3 --enable-demuxer=ogg --enable-demuxer=wav --enable-muxer=ogg --enable-muxer=opus
 
 	make
 	install_and_mark ffmpeg
+fi
+
+if ! already_done portaudio; then
+    # They have no option to get 'latest stable' release. So hardcode it.
+    pa_tar="pa_stable_v190600_20161030.tgz"
+    prepare_source_from_http portaudio "$pa_tar" "http://www.portaudio.com/archives/$pa_tar"
+    ./configure
+    make
+    install_and_mark portaudio
+fi
+
+if ! already_done openal-soft; then
+    prepare_source_from_git openal-soft "git://repo.or.cz/openal-soft.git"
+    cd build
+    inst cmake
+    cmake -D LIBTYPE:STRING=STATIC ..
+    make
+    install_and_mark openal-soft
 fi
 
