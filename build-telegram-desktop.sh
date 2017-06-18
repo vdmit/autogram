27f8c6c3b2a453b72fdda0bf5c9e1c4ba911ad17
Dmitry Veltishchev <vdm-photo@ya.ru>
2017-06-18 21:41:22 +0300

    ffmpeg & Co. done

--- build-telegram-desktop.sh
+++ build-telegram-desktop.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# all-in-one Telegram Desktop build script for Linux
+# Tested on Ubuntu series (16.04 LTS)
+
+# (c) 2017 Dmitry Veltishchev <dm.velt@ya.ru> (github:vdmit)
+# See LICENSE.md for legal info
+
 set -e
 
 inst()
@@ -34,6 +40,7 @@ install_and_mark()
         exit 1
     fi
     minstall
+	log "leaving $(basename `pwd`)"
     cd ..
     touch "$1.done"
 }
@@ -91,17 +98,18 @@ prepare_source_from_git()
         exit 1
     fi
 
-    if ! [ -d "${dir}" ]; then
-		git clone "$url"
+    if ! [ -d "$dir" ]; then
+		git clone "$url" "$dir"
     else
-        log "${dir} already exists, skipping clone operation"
+        log "$dir already exists, skipping clone operation"
     fi
+	log "entering $dir"
     cd "$dir"
 }
 
 inst git
 inst g++
-inst libtool automake
+inst libtool automake autoconf build-essential
 inst wget
 
 # root directory for all stuff
@@ -146,5 +154,35 @@ if ! already_done opus; then
     ./configure
     make
     install_and_mark opus
+fi
+
+log "building FFmpeg and Co."
+
+if ! already_done libva; then
+    prepare_source_from_git "libva" "https://github.com/01org/libva.git"
+
+	./autogen.sh --enable-static
+	make
+	install_and_mark libva
+fi
+
+if ! already_done libvdpau; then
+    prepare_source_from_git "libvdpau" "git://anongit.freedesktop.org/vdpau/libvdpau"
+	./autogen.sh --enable-static
+	make
+	install_and_mark libvdpau
+fi
+
+if ! already_done ffmpeg; then
+	prepare_source_from_git ffmpeg "https://github.com/FFmpeg/FFmpeg.git" ffmpeg
+	git checkout "release/3.2"
+
+	inst libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
+	inst yasm
+
+	./configure --prefix=/usr/local --disable-programs --disable-doc --disable-everything --enable-protocol=file --enable-libopus --enable-decoder=aac --enable-decoder=aac_latm --enable-decoder=aasc --enable-decoder=flac --enable-decoder=gif --enable-decoder=h264 --enable-decoder=h264_vdpau --enable-decoder=mp1 --enable-decoder=mp1float --enable-decoder=mp2 --enable-decoder=mp2float --enable-decoder=mp3 --enable-decoder=mp3adu --enable-decoder=mp3adufloat --enable-decoder=mp3float --enable-decoder=mp3on4 --enable-decoder=mp3on4float --enable-decoder=mpeg4 --enable-decoder=mpeg4_vdpau --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 --enable-decoder=opus --enable-decoder=pcm_alaw --enable-decoder=pcm_alaw_at --enable-decoder=pcm_f32be --enable-decoder=pcm_f32le --enable-decoder=pcm_f64be --enable-decoder=pcm_f64le --enable-decoder=pcm_lxf --enable-decoder=pcm_mulaw --enable-decoder=pcm_mulaw_at --enable-decoder=pcm_s16be --enable-decoder=pcm_s16be_planar --enable-decoder=pcm_s16le --enable-decoder=pcm_s16le_planar --enable-decoder=pcm_s24be --enable-decoder=pcm_s24daud --enable-decoder=pcm_s24le --enable-decoder=pcm_s24le_planar --enable-decoder=pcm_s32be --enable-decoder=pcm_s32le --enable-decoder=pcm_s32le_planar --enable-decoder=pcm_s64be --enable-decoder=pcm_s64le --enable-decoder=pcm_s8 --enable-decoder=pcm_s8_planar --enable-decoder=pcm_u16be --enable-decoder=pcm_u16le --enable-decoder=pcm_u24be --enable-decoder=pcm_u24le --enable-decoder=pcm_u32be --enable-decoder=pcm_u32le --enable-decoder=pcm_u8 --enable-decoder=pcm_zork --enable-decoder=vorbis --enable-decoder=wavpack --enable-decoder=wmalossless --enable-decoder=wmapro --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmavoice --enable-encoder=libopus --enable-hwaccel=h264_vaapi --enable-hwaccel=h264_vdpau --enable-hwaccel=mpeg4_vaapi --enable-hwaccel=mpeg4_vdpau --enable-parser=aac --enable-parser=aac_latm --enable-parser=flac --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mpegaudio --enable-parser=opus --enable-parser=vorbis --enable-demuxer=aac --enable-demuxer=flac --enable-demuxer=gif --enable-demuxer=h264 --enable-demuxer=mov --enable-demuxer=mp3 --enable-demuxer=ogg --enable-demuxer=wav --enable-muxer=ogg --enable-muxer=opus
+
+	make
+	install_and_mark ffmpeg
 fi
 
